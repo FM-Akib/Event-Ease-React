25253b2065c7aa127174e111bd533cd15b46ede3
FM-Akib <muntasirakib08@gmail.com>
2024-07-15 15:45:28 +0600

    added edithall

--- src/Pages/HallDashboard/EditHallProfile.jsx
+++ src/Pages/HallDashboard/EditHallProfile.jsx
@@ -1,14 +1,14 @@
 
 const EditHallProfile = () => {
     return (
-<div className=" border  rounded-lg shadow relative p-10">
+<div className=" border relative p-10">
 
     <div className="flex items-start justify-between p-5 border-b rounded-t">
         <h3 className="text-xl font-semibold">
             Edit your hall info
         </h3>
         <button type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center" data-modal-toggle="product-modal">
-        <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+        <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
         </button>
     </div>
 
@@ -16,8 +16,8 @@ const EditHallProfile = () => {
         <form action="#">
             <div className="grid grid-cols-6 gap-6">
                 <div className="col-span-6 sm:col-span-3">
-                    <label  className="text-sm font-medium text-gray-900 block mb-2">Product Name</label>
-                    <input type="text" name="product-name" id="product-name" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-cyan-600 focus:border-cyan-600 block w-full p-2.5" placeholder="Apple Imac 27”" required=""/>
+                    <label  className="text-sm font-medium text-gray-900 block mb-2">Hall Name</label>
+                    <input type="text" name="hallname"  className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-cyan-600 focus:border-cyan-600 block w-full p-2.5" placeholder="Apple Imac 27”" required=""/>
                 </div>
                 <div className="col-span-6 sm:col-span-3">
                     <label  className="text-sm font-medium text-gray-900 block mb-2">Category</label>
@@ -40,7 +40,7 @@ const EditHallProfile = () => {
     </div>
 
     <div className="p-6 border-t border-gray-200 rounded-b">
-        <button className="text-white bg-cyan-600 hover:bg-cyan-700 focus:ring-4 focus:ring-cyan-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center" type="submit">Save all</button>
+        <button className="text-white bg-orange-600 hover:bg-cyan-700 focus:ring-4 focus:ring-cyan-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center" type="submit">Update</button>
     </div>
 
 </div>
